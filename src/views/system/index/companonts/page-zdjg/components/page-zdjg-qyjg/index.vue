5e058b1cd64e5b41a4201ab9c34eeb593e555c49
林琦海 <linqihai1@xiaoyouzi.com>
2020-11-23 23:13:36 +0800

    区域监管

---
+++ src/views/system/index/companonts/page-zdjg/components/page-zdjg-qyjg/index.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="page-zdjg-qyjg" >
+        <header-crumb :first-item="firstItem" :second-item="secondItem"></header-crumb>
+        <div flex-box="1" class="body">
+            <div class="column-1" >
+                <div style="padding: 16px; position: relative">
+                    <el-input placeholder="地区名称" class="search-input"></el-input>
+                    <button class="search-button"><span>搜索</span></button>
+                    <button class="create-button"><span>添加</span></button>
+                </div>
+                <div>
+                    <el-main v-loading="loading" element-loading-background="rgba(0, 0, 0, 0.5)">
+                        <el-table class="custom-table" :data="tableData">
+                            <el-table-column align="center" prop="area_name" label="地区名称" minWidth="150"></el-table-column>
+                            <el-table-column align="center" prop="" label="操作">
+                                <template slot-scope="scope">
+                                    <el-button type="text" size="mini">修改</el-button>
+                                    <el-button type="text" size="mini">删除</el-button>
+                                </template>
+                            </el-table-column>
+                        </el-table>
+                    </el-main>
+                    <div  class="button-page-group">
+                        <button class="button-page" size="mini" icon="el-icon-arrow-up">上一页</button>
+                        <button class="button-page" size="mini"  icon="el-icon-arrow-down">下一页</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import HeaderCrumb from '../common/header-crumb'
+  export default {
+    name: 'index',
+    props:{
+      firstItem:{
+        type:String,
+        required:false,
+        default:''
+      },
+      secondItem:{
+        type:String,
+        required:false,
+        default:''
+      }
+    },
+    components:{
+      HeaderCrumb
+    },
+    data(){
+      return {
+        loading:false,
+        tableData:[
+          {
+            area_name:"海淀区上地8街12号"
+          }
+        ],
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    .body {
+        margin-top: 8px;
+        width: 100%;
+        display: flex;
+        .column-1 {
+            width: 400px;
+            height: 100%;
+            display: inline-block;
+            min-height: 100px;
+            background: rgba(0, 115, 255, 0.2);
+            border-radius: 4px;
+
+            position: relative;
+        }
+    }
+
+</style>
--- src/views/system/index/companonts/page-zdjg/index.vue
+++ src/views/system/index/companonts/page-zdjg/index.vue
@@ -1,15 +1,45 @@
 <template>
+    <div class="page-zdjg">
+        <div class="custom-menu-box">
+        <span class="menu" v-for="(option,index) in pageOptions" :key="index"
+            :class="curPage===option?'is-active':''" @click="curPage=option">{{option.label}}</span>
+        </div>
+        <page-zdjg-qyjg  :first-item="firstItem" :second-item="curPage.label" v-show="curPage.label=='区域监管'" class="custom-main-content"></page-zdjg-qyjg>
+    </div>
 
 </template>
 
 <script>
+    import './common.scss'
+  import PageZdjgQyjg from './components/page-zdjg-qyjg'
   export default {
-    components: {},
+    components: {
+      PageZdjgQyjg,
+    },
     data(){
-      return {}
+      return {
+        curPage: {},
+        firstItem:"重点监管",
+        pageOptions: [
+          {label: '区域监管'},
+          {label: '路段监管'},
+          {label: '车辆布控'},
+          {label: '特殊车辆监管'},
+          {label: '牌照异常'},
+        ],
+      }
     },
     mounted(){
+      this.curPage = this.pageOptions[0]
     },
     methods: {}
   }
 </script>
+
+<style lang="scss">
+    .page-zdjg {
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
+</style>
