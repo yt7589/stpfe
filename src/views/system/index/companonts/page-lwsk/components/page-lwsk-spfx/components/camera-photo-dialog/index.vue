7f4c2316f1de04840728fc222f5686df5b0c7cae
yantao <yt7589@qq.com>
2021-01-13 21:12:44 +0800

    fix BUG20210113001

--- src/views/system/index/companonts/page-lwsk/components/page-lwsk-spfx/components/camera-photo-dialog/index.vue
+++ src/views/system/index/companonts/page-lwsk/components/page-lwsk-spfx/components/camera-photo-dialog/index.vue
@@ -4,7 +4,7 @@
       <el-image style="width:100%;height:80%" :src="require('../../image/lwsk_spfx_main.jpg')"></el-image>
       <ul class="item-box" id="image-box">
         <li class="li-image" v-for="item in spfxs" :key="item.seq">
-          <el-image :src="require('{{item.image}}')"></el-image>
+          <el-image :src="require('item.image')"></el-image>
           <div>
             <div style="display: flex;justify-content: space-between;">
               <span>{{item.hphm}}</span>
