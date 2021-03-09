d21b9a76e852ccfaf35d1761f81dc09e732fa957
ps <mickey-cool@126.com>
2021-03-09 21:11:07 +0800

    swith to pure spring boot mode

--- src/app_global.js
+++ src/app_global.js
@@ -3,11 +3,11 @@
  * 定义微服务名称
  */
 // 直接使用微服务
-/*const msFacade = ''
-const msTmdp = '' */
+const msFacade = ''
+const msTmdp = ''
 // 使用应用网关
-const msFacade = 'ms-facade/'
-const msTmdp = 'ms-tmdp/'
+//const msFacade = 'ms-facade/'
+//const msTmdp = 'ms-tmdp/'
 
 
 export default {
